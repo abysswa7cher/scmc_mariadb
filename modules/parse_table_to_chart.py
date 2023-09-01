ab78f45ad94f6afad63bc9871e7133e385579ba5
Abysswa7cher <abysswa7cher@gmail.com>
2023-09-01 23:47:44 +0200

    1

--- modules/parse_table_to_chart.py
+++ modules/parse_table_to_chart.py
@@ -11,22 +11,9 @@ def construct_price_timeframe(db, table_name, date):
     if date is not None:
         if type(date) == tuple:
             year = date[0] if len(date) > 0 else "*"
-            print(f"year: {year}")
-
             month = date[1] if len(date) > 1 else "*"
-            print(f"month: {month}")
-
             day = date[2] if len(date) > 2 else "*"
-            print(f"day: {day}")
-
             hour = date[3] if len(date) > 3 else "*"
-            print(f"day: {hour}")
-        # elif type(date) == str:
-        #     year = date
-        #     month, day, hour, = "*", "*", "*"
-        #     print(f"Only year {year} was selected.")
-
-    print((year, month, day, hour))
 
     query = "select price, quantity, id, year, month, day, hour, minutes from {}".format(table_name)
     query = (query + (" where year='{}'" .format(year)   if year  != "*" else "") + 
@@ -127,16 +114,51 @@ def draw_chart(db, table_name=None, date=None):
 
     db.disconnect()
 
+    #macd
+    ewm_12 = df["Close"].ewm(span=12, adjust=False).mean(),
+    ewm_26 = df["Close"].ewm(span=26, adjust=False).mean()
+    macd = ewm_12-ewm_26.mean()
+    df["MACD"] = macd[0]
+
+    #rsi
+    delta = df['Close'].diff()
+    period = 14
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+    avg_gain = gain.rolling(period).mean()
+    avg_loss = loss.rolling(period).mean()
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    df['RSI'] = rsi
+#region
+    line33=[0.333 for x in range(0, len(df.index))]
+    line33=pd.DataFrame([line33]).T
+    line77=[0.777 for x in range(0, len(df.index))]
+    line77=pd.DataFrame([line77]).T
+    print(line33)
+    print(line77)
+#endregion
+    ap0 = [fplt.make_addplot(df["MACD"], color="b", panel=1, ylabel="MACD"),
+           fplt.make_addplot(df["RSI"], color="g", panel=2, ylabel="RSI"),
+           fplt.make_addplot(line33, panel=2, color='b'),
+           fplt.make_addplot(line77, panel=2, color='r')]
+    
     df_date = df.index[-1]
 
     buffer = io.BytesIO()
     img_name = f"{table_name} ({df_date})"
-    fplt.plot(  df,
+    fplt.plot(  df, addplot=ap0,
                 type='candle',
                 style='yahoo',
                 title=img_name,
                 ylabel='Price ($)', 
-                mav=(7, 21), tight_layout=True, volume=True, savefig=buffer)#, , figratio=(10, 6)
+                mav=(3, 9, 12),
+                tight_layout=True, 
+                volume=False, 
+                figratio=(16, 9),
+                figscale=0.8,
+                savefig=buffer)
+    
     buffer.seek(0)
     buf_png = base64.b64encode(buffer.getvalue()).decode()
 
